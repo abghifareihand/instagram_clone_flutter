fd154197aba0ae3a490e635900e1ba3ff8c209f4
abghifareihand <info.abghifareihan@gmail.com>
2023-01-26 14:07:04 +0700

    setting firebase mobile and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
+import 'package:instagram_flutter/responsive/responsive_layout.dart';
+import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyDkbSanZWE1uhrpkzxvWFwpmiajdtTIM7I',
+        appId: '1:1022236480103:web:8dd928bb26433d730cbac2',
+        messagingSenderId: '1022236480103',
+        projectId: 'instagram-clone-flutter-846fe',
+        storageBucket: 'instagram-clone-flutter-846fe.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -15,6 +35,10 @@ class MyApp extends StatelessWidget {
       title: 'Instagram Clone',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
       ),
     );
   }
